9a4dd628aea832ca19127f2edb2bedcd33ef95eb
utkarsh bhaskar <utkarshbhaskar05@gmail.com>
2021-10-10 03:37:15 +0530

    tons of changes

---
+++ login/login.js
@@ -0,0 +1,10 @@
+var uiConfig = {
+  signInSuccessUrl: 'index.html',
+  signInOptions: [
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+
+  tosUrl: 'index.html'
+};
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+ui.start('#firebaseui-auth-container', uiConfig);
