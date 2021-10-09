1158fbdced714ad7d8e688e2781d06d9aa165421
Suyog Shinde <73709524+shindesuyog@users.noreply.github.com>
2021-10-09 20:01:54 +0530

    Add files via upload

---
+++ quiz/js/questions.js
@@ -0,0 +1,69 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Suppose you have $100 in a savings account earning 2 percent interest a year. After five years, how much would you have?",
+    answer: "More than $102",
+    options: [
+      "More than $102",
+      "Exactly $102",
+      "Less than $102",
+      "Don't Know"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Imagine that the interest rate on your savings account is 1 percent a year and inflation is 2 percent a year. After one year, would the money in the account buy more than it does today, exactly the same or less than today?",
+    answer: "Less",
+    options: [
+      "More",
+      "Same",
+      "Less",
+      "Don't Know"
+    ]
+  },
+    {
+    numb: 3,
+    question: "If interest rates rise, what will typically happen to bond prices? Rise, fall, stay the same, or is there no relationship?",
+    answer: "Fall",
+    options: [
+      "Rise",
+      "Fall",
+      "Stay the Same",
+      "No Relationship"
+    ]
+  },
+    {
+    numb: 4,
+    question: "True or false: A 15-year mortgage typically requires higher monthly payments than a 30-year mortgage but the total interest over the life of the loan will be less.",
+    answer: "True",
+    options: [
+      "True",
+      "False",
+      "Don't Know",
+      ""
+    ]
+  },
+    {
+    numb: 5,
+    question: "True or false: Buying a single company's stock usually provides a safer return than a stock mutual fund.",
+    answer: "False",
+    options: [
+      "True",
+      "False",
+      "Don't Know",
+      ""
+    ]
+  },
+  {
+    numb: 2,
+    question: "Suppose you owe $1,000 on a loan and the interest rate you are charged is 20% per year compounded annually. If you didn't pay anything off, at this interest rate, how many years would it take for the amount you owe to double?",
+    answer: "2 to 4 years",
+    options: [
+      "Less than 2 years",
+      "2 to 4 years",
+      "5 to 9 years",
+      "Don't Know"
+    ]
+  },
+];
